d77b149f87540aa7cc52b2c1c320b1527d7e18fb
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-24 00:38:47 +0600

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h2>doctors portal</h2>
+      <button className="btn btn-ghost">Button</button>
     </div>
   );
 }
