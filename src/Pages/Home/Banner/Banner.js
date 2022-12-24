77a973e71d481077528ea52698341f94f9c839b5
mimnets <mdmonirulislammonir@gmail.com>
2022-12-24 20:20:35 +0600

    navbar image and text fixed

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import chair from '../../../assets/images/chair.png';
 const Banner = () => {
     return (
         <div className="hero">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://placeimg.com/260/400/arch" className="rounded-lg shadow-2xl" alt=""/>
-                <div className='bg-hero-image w-full'>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                <img src={chair} className="w-1/2 rounded-lg shadow-2xl" alt=""/>
+                <div className="">
+                    <h1 className="text-5xl font-bold">Your New Smile Starts <br/> Here</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">Get Started</button>
                 </div>
             </div>
         </div>
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
     </React.Fragment>
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 flex justify-between">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -29,9 +29,6 @@ const Navbar = () => {
                 <ul className="menu menu-horizontal px-1">
                     {menuItems}
                 </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Get started</a>
             </div>
         </div>
     );
