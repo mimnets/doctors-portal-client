347b316c56fe57110b836e8759a321a0cb79a959
mimnets <mdmonirulislammonir@gmail.com>
2022-12-24 19:59:10 +0600

    fix the banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="hero">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src="https://placeimg.com/260/400/arch" className="rounded-lg shadow-2xl" alt=""/>
+                <div className='bg-hero-image w-full'>
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,25 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui:{
+    themes:[
+      {
+        doctortheme:{
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        }
+      }
+    ]
+  },
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'hero-image': "url('/src/assets/images/bg.png')"
+      }
+    },
   },
   plugins: [
     require("daisyui")
