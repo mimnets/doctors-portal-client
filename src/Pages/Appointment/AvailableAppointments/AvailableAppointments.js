e8e7946d1705f9a5fb794ec6d8cf2385b7adbcab
mimnets <mdmonirulislammonir@gmail.com>
2022-12-26 19:28:59 +0600

    updated available option

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,10 +1,24 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOptions from './AppointmentOptions';
 
 const AvailableAppointments = ({selectedDate}) => {
+    const [appointmentOptions, setAppointmentOptions] = useState();
+    console.log(appointmentOptions)
+
+    useEffect(() =>{
+        fetch('appointmentOptions.json')
+        .then(res => res.json())
+        .then(data => setAppointmentOptions(data))
+      },[])
     return (
         <section className='mt-16'>
             <p className='text-center text-secondary font-bold'>Available Appointments on {format(selectedDate, 'PP')}</p>
+            <div>
+                {
+                    appointmentOptions.map(appointmentOption => <AppointmentOptions key={appointmentOption._id}></AppointmentOptions>)
+                }
+            </div>
         </section>
     );
 };
