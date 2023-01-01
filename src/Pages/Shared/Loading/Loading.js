13642c800762dd27812e755943314733346ea451
mimnets <mdmonirulislammonir@gmail.com>
2023-01-01 11:09:03 +0600

    change class to className at loading

--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Loading = () => {
     return (
 
-        <div class="h-screen bg-white">
-            <div class="flex justify-center items-center">
-                <img class="h-16 w-16" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt="" />
+        <div className="h-screen bg-white">
+            <div className="flex justify-center items-center">
+                <img className="h-16 w-16" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt="" />
             </div>
         </div>
     );
