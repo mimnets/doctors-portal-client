3d96ab4aca70963dea948576a687b1db1487a8f6
mimnets <mdmonirulislammonir@gmail.com>
2023-01-03 14:51:59 +0600

    added admin route to protect admin page

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider';
+import useAdmin from '../hooks/useAdmin';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
+    const {user} = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email);
+
+
     return (
         <div>
             <Navbar></Navbar>
@@ -16,7 +22,12 @@ const DashboardLayout = () => {
                     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
                         {/* <!-- Sidebar content here --> */}
                         <li><Link to='/dashboard'>My Appointments</Link></li>
-                        <li><Link to='/dashboard/all-users'>All Users</Link></li>
+                        {
+                            isAdmin && 
+                            <>
+                                <li><Link to='/dashboard/all-users'>All Users</Link></li>
+                            </>
+                        }
                     </ul>
 
                 </div>
