a14a8790a686d687bb09f443ec6a4aac82b2e152
Monirul Islam <mdmonirulislammonir@gmail.com>
2023-01-04 23:53:31 +0600

    show doctors list to manage doctors

--- src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,10 +1,69 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from '../../Shared/Loading/Loading';
 
 const ManageDoctors = () => {
-    
+    const { data: doctors, isLoading } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:5000/doctors', {
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem('accessToken')}`
+                    }
+                })
+                const data = await res.json()
+                return data;
+            }
+            catch (err) {
+                console.log(err);
+            }
+        },
+    })
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h3 className="text-4xl">Manage Doctors</h3>
+            <h3 className="text-4xl">Manage Doctors: {doctors.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            doctors.map((doctor, i) =>
+                                <tr key={doctor._id}>
+                                    <th>{i + 1}</th>
+                                    <td>
+                                        <div className="avatar">
+                                            <div className="w-24 rounded-full">
+                                                <img src={doctor.image} alt=""/>
+                                            </div>
+                                        </div>
+
+                                    </td>
+                                    <td>{doctor.name}</td>
+                                    <td>{doctor.email}</td>
+                                    <td>{doctor.specialty}</td>
+                                    <td><button className='btn btn-error'>Delete</button></td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
