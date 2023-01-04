030cc7848896141da56cedf0be3f23d79d9a1bb4
Monirul Islam <mdmonirulislammonir@gmail.com>
2023-01-04 22:31:12 +0600

    remove isLoading from MyAppointmnet due to uncaught error with bookings.map

--- src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -8,7 +8,7 @@ const MyAppointment = () => {
 
     const url = `http://localhost:5000/bookings?email=${user?.email}`
 
-    const { data: bookings = [], isLoading } = useQuery({
+    const { data: bookings = []} = useQuery({
         queryKey: ['bookings', user?.email],
         queryFn: async () => {
             const res = await fetch(url, {
@@ -21,9 +21,6 @@ const MyAppointment = () => {
         },
     })
 
-    if(isLoading){
-        return <Loading></Loading>
-    }
 
     return (
         <div>
@@ -41,7 +38,7 @@ const MyAppointment = () => {
                     </thead>
                     <tbody>
                         {
-                            bookings.map((booking, i) =>
+                            bookings?.map((booking, i) =>
                                 <tr key={booking._id}>
                                     <th>{i}</th>
                                     <td>{booking.patient}</td>
--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
     return (
 
-        <div className="h-screen bg-white">
+        <div className="bg-white">
             <div className="flex justify-center items-center">
                 <img className="h-16 w-16" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt="" />
             </div>
