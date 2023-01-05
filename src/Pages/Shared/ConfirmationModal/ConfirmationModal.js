260916198b6e1363db0d8a61422607766796a4a4
Monirul Islam <mdmonirulislammonir@gmail.com>
2023-01-06 00:09:01 +0600

    Delete successfully a doctor

--- src/Pages/Shared/ConfirmationModal/ConfirmationModal.js
+++ src/Pages/Shared/ConfirmationModal/ConfirmationModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ConfirmationModal = ({title, message, closeModal, modalData, successAction}) => {
+const ConfirmationModal = ({title, message, closeModal, successButtonName, modalData, successAction}) => {
     return (
         <div>
             <input type="checkbox" id="confirmation-modal" className="modal-toggle" />
@@ -9,7 +9,11 @@ const ConfirmationModal = ({title, message, closeModal, modalData, successAction
                     <h3 className="font-bold text-lg">{title}</h3>
                     <p className="py-4">{message}</p>
                     <div className="modal-action">
-                        <label onClick={() => successAction(modalData)} htmlFor="confirmation-modal" className="btn">Yes</label>
+                        <label 
+                        onClick={() => successAction(modalData)}
+                         htmlFor="confirmation-modal" 
+                         className="btn btn-success"
+                         >{successButtonName}</label>
                         <button onClick={closeModal} className='btn btn-outline'>Cancel</button>
                     </div>
                 </div>
