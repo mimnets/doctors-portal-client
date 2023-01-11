6550084e039fd2dc2068454999463d0a2ef971f8
Monirul Islam <mdmonirulislammonir@gmail.com>
2023-01-11 23:30:27 +0600

    added stripe payment form

--- src/Pages/Dashboard/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Dashboard/Payment/Payment.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+import CheckoutForm from './CheckoutForm';
 
+
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+// console.log(stripePromise);
 const Payment = () => {
     const booking = useLoaderData();
-    const {treatment, price, appointmentDate, slot} = booking;
+    const { treatment, price, appointmentDate, slot } = booking;
     console.log(booking)
     return (
         <div>
             <h2 className='text-3xl'>Payment for {treatment}</h2>
             <p className='text-xl'>Please pay <strong>${price}</strong> for your appointment on {appointmentDate} at {slot}.</p>
+
+            <div className='w-96 my-6'>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
+            </div>
         </div>
     );
 };
