5561e7c074f84756215a0c61f38a9a22b6001f7b
Monirul Islam <mdmonirulislammonir@gmail.com>
2023-01-13 00:42:35 +0600

    client side completed successfully for doctors portal

--- src/Pages/Dashboard/Dashboard/Payment/CheckoutForm.js
+++ src/Pages/Dashboard/Dashboard/Payment/CheckoutForm.js
@@ -1,58 +1,137 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ booking }) => {   
+  const { price, email, patient, _id } = booking;
   const [cardError, setCardError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [processing, setProcessing] = useState(false);
+  const [transactionId, setTransactionId] = useState('');
+  const [clientSecret, setClientSecret] = useState('');
   const stripe = useStripe()
   const elements = useElements();
 
+  useEffect(() => {
+    fetch('http://localhost:5000/create-payment-intent', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `bearer ${localStorage.getItem('accessToken')}}`
+      },
+      body: JSON.stringify({ price }),
+    })
+      .then(res => res.json())
+      .then(data => setClientSecret(data.clientSecret))
+  }, [price])
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-    if(!stripe || !elements){
-      return; 
+    if (!stripe || !elements) {
+      return;
     }
 
     const card = elements.getElement(CardElement);
 
-    if(card === null){
+    if (card === null) {
       return;
     }
 
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
-      type:'card',
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
       card,
     });
 
-    if(error){
+    if (error) {
       // console.log(error);
       setCardError(error.message);
     }
-    else{
+    else {
       setCardError('');
     }
+
+    setSuccess('');
+    setProcessing(true);
+
+    const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+      clientSecret,
+      {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: patient,
+            email: email,
+          },
+        },
+      },
+    );
+    if(confirmError) {
+      setCardError(confirmError.message);
+      return;
+    }
+    if(paymentIntent.status === 'succeeded'){
+      // console.log(card);
+      // Store payment info in the database
+      const payment = {
+        price,
+        email,
+        transactionId: paymentIntent.id, 
+        bookingId: _id,
+      }
+
+      fetch('http://localhost:5000/payments',{
+        method: 'POST',
+        headers:{
+          "content-type": "application/json",
+          authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        },
+        body: JSON.stringify(payment),
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if(data.insertedId){
+          setSuccess('Congrats! Your payment has been confirmed');
+          setTransactionId(paymentIntent.id)
+        }
+      })
+    }
+
+    setProcessing(false); 
+    // console.log(paymentIntent);
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: '16px',
+                color: '#424770',
+                '::placeholder': {
+                  color: '#aab7c4',
+                },
+              },
+              invalid: {
+                color: '#9e2146',
               },
             },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button className='btn btn-sm btn-primary mt-4' type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <button className='btn btn-sm btn-primary mt-4' type="submit"
+          disabled={!stripe || !clientSecret || processing}
+        >
+          Pay
+        </button>
+      </form>
+      <p className="text-red-500">{cardError}</p>
+      {
+        success && <div>
+          <p className='text-green-500'>{success}</p>
+          <p>Your TransactionId: <span className='font-bold'>{transactionId}</span></p>
+        </div>
+      }
+    </>
   );
 };
 
--- src/Pages/Dashboard/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Dashboard/Payment/Payment.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigation } from 'react-router-dom';
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import CheckoutForm from './CheckoutForm';
+import Loading from '../../../Shared/Loading/Loading';
 
 
 
@@ -10,7 +11,12 @@ const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 // console.log(stripePromise);
 const Payment = () => {
     const booking = useLoaderData();
+    const navigation = useNavigation();
+
     const { treatment, price, appointmentDate, slot } = booking;
+    if(navigation.state === 'loading') {
+        return <Loading></Loading>
+    }
     console.log(booking)
     return (
         <div>
@@ -19,7 +25,9 @@ const Payment = () => {
 
             <div className='w-96 my-6'>
                 <Elements stripe={stripePromise}>
-                    <CheckoutForm />
+                    <CheckoutForm 
+                    booking={booking}
+                    />
                 </Elements>
             </div>
         </div>
